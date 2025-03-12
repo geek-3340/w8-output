be79189d9504c664e8755f8abf0e1d2fec20cfe2
GEEK <crew.sento0864@gmail.com>
2025-03-12 03:45:44 +0000

    ＪＳ更新（仮

--- html-css/main.js
+++ html-css/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // フォーム要素の取得
   const nameInput = document.getElementById("js-email");
   const emailInput = document.getElementById("js-password");
-  const form = document.getElementById("js-form");
+  const button = document.getElementById("js-button");
 
   // 以前のデータがあればフォームに復元
   if (localStorage.getItem("userData")) {
@@ -11,18 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
     emailInput.value = savedData.email;
   }
 
-  // 入力内容をリアルタイムで保存
-  form.addEventListener("input", () => {
+  // // 入力内容をリアルタイムで保存
+  // button.addEventListener("click", () => {
+    
+  // });
+
+  // 送信時の処理（今回はデータの保存のみ）
+  form.addEventListener("submit", (event) => {
     const userData = {
       name: nameInput.value,
       email: emailInput.value
     };
     localStorage.setItem("userData", JSON.stringify(userData));
-  });
-
-  // 送信時の処理（今回はデータの保存のみ）
-  form.addEventListener("submit", (event) => {
-    event.preventDefault(); // フォーム送信を防止
     alert("データが保存されました！");
   });
   
